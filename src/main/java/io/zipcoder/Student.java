a2ab7753f404c738aa951828ccb190dfa9cc9374
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-03-13 14:36:49 -0400

    This is right up my alley

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,14 @@
 package io.zipcoder;
 
-public class Student {
+import java.util.ArrayList;
+
+public class Student extends Classroom{
+    public Student(String firstName, String lastName, ArrayList<Double> testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.testScores = testScores;
+    }
+   private String firstName ;
+   private String lastName ;
+   private ArrayList<Double> testScores;
 }
